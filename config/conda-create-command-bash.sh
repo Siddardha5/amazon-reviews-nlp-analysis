34b15516832e84f6ae27831afb94984d002a86fa
James M. Irving, Ph.D. <james.irving.phd@gmail.com>
2024-08-21 17:19:54 -0400

    env creation scripts added

--- config/conda-create-command-bash.sh
+++ config/conda-create-command-bash.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
+# Update base conda
+conda update -n base conda -y
+conda install -y -n base conda-libmamba-solver
+conda update -y -n base conda-libmamba-solver
+conda config --set solver libmamba
 
 # Create a new conda environment named 'nlp_project' with Python 3.8
-conda create -n nlp-env python=3.10 -y
+conda create -n nlp-env -y python=3.11  pandas numpy matplotlib seaborn nltk gensim pyldavis  scikit-learn imbalanced-learn missingno seaborn jupyter notebook  #tabulate pydantic faiss-cpu plotly 
 
 # Activate the new environment
-source activate nlp-env
+conda init bash
+source activate nasa-env
 
 
 # Pip install custom package
 pip install dojo-ds
 
 # Install the required packages
-conda install -y pandas numpy matplotlib seaborn nltk gensim pyldavis jupyter scikit-learn
+# conda install -y pandas numpy matplotlib seaborn nltk gensim pyldavis jupyter scikit-learn
 
 # Install the kenrel in jupyter
-python -m ipykernel install --user --name=nlp-env --display-name="Python(nlp-env)"
+python -m ipykernel install --user --name=nasa-env 
 
 # Additional installations if required
 # conda install -y <other-packages>
@@ -23,4 +29,4 @@ python -m ipykernel install --user --name=nlp-env --display-name="Python(nlp-env
 # Deactivate the environment
 conda deactivate
 
-echo "Environment 'nlp_project' created and packages installed successfully."
+echo "Environment 'nasa-env' created and packages installed successfully."
--- config/conda-create-command-zsh.sh
+++ config/conda-create-command-zsh.sh
@@ -3,9 +3,17 @@
 # Usage:
 # 1. Make executable: chmod +x conda-create-command-zsh.sh
 # 2. Run: ./conda-create-command-zsh.sh
-# Create a new conda environment named 'nlp-env' with Python 3.10
-conda create -n nlp-env python=3.10 -y
+# Update base conda
+conda update -n base conda -y
+conda install -y -n base conda-libmamba-solver
+conda update -y -n base conda-libmamba-solver
+conda config --set solver libmamba
 
+# Create a new conda environment named 'nlp-env' with Python 3.10
+# conda create -n nlp-env -y python"=3.12" jupyter pandas numpy matplotlib seaborn nltk gensim pyldavis  scikit-learn
+conda create -n nlp-env -y python=3.11  pandas numpy matplotlib seaborn nltk gensim pyldavis  scikit-learn imbalanced-learn missingno seaborn jupyter notebook  #tabulate pydantic faiss-cpu plotly 
+
+# 
 # Initialize conda for zsh (only needed if not already done)
 conda init zsh
 
@@ -16,28 +24,32 @@ source ~/.zshrc
 conda activate nlp-env
 
 # Pip install custom package
-pip install dojo-ds
+# pip install dojo-ds
 
 # Install the required packages
-conda install -y pandas numpy matplotlib seaborn nltk gensim pyldavis jupyter scikit-learn
+# conda install -y jupyter pandas numpy matplotlib seaborn nltk gensim pyldavis  scikit-learn
 
 # Install the kernel in jupyter
-python -m ipykernel install --user --name=nlp-env --display-name="Python(nlp-env)"
+python -m ipykernel install --user --name=nlp-env
 
 # Additional installations if required
 # conda install -y <other-packages>
 # pip install <other-pip-packages>
 
-conda install -c conda-forge spacy
-python -m spacy download en_core_web_sm
+# conda install -c conda-forge spacy
+# python -m spacy download en_core_web_sm
 
+# pip install missingno seaborn tabulate 
+# pip install imbalanced-learn
+# pip install langchain pydantic langchain_openai langchain_core langchain_community faiss-cpu
+# pip install tensorflow-macos
+# pip install tensorflow-metal
+# pip install -U protobuf
+# pip install transformers
+# pip install streamlit plotly
 pip install scipy==1.10.1
-pip install missingno seaborn tabulate
-pip install pydantic langchain_openai langchain_core
-pip install tensorflow-macos
-pip install tensorflow-metal
-pip install -U protobuf
-pip install transformers
+python3 -m pip install tensorflow-macos
+
 # pip install tf-keras
 
 # Deactivate the environment
